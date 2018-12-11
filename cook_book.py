bf366e00cef8d814fa141b02806b028ed9340b19
egorandreevich <45241457+egorandreevich@users.noreply.github.com>
2018-12-11 01:30:10 +0000

    Add files via upload

---
+++ cook_book.py
@@ -0,0 +1,30 @@
+with open('cooking_list.txt', 'r') as f:
+    cook_book = {}
+
+    def create_cook_book():
+        for line in f:
+            dish_name = line.strip()
+            ing_amount = f.readline()
+            ing_list = []
+            for data in range(int(ing_amount)):
+                parts = f.readline().split('|')
+                ing_dict = {'ingridient_name': parts[0].strip(), 'quantity': parts[1], 'measure': parts[2].strip()}
+                ing_list.append(ing_dict)
+            empty_line = f.readline()
+            cook_book[dish_name] = ing_list
+        return cook_book
+
+    def get_shop_list_by_dishes(dishes, person_count):
+        for dish in dishes:
+            for key, value in cook_book.items():
+                shop_list = {}
+                if key in dish:
+                    shop_list = cook_book.get(dish)
+                    for ingr in shop_list:
+                        shop_list_dict = {}
+                        shop_list_dict[ingr['ingridient_name']] = {'measure': ingr['measure'], 'quantity': (
+                                    int(ingr['quantity']) * (int(person_count)))}
+                        print(shop_list_dict)
+
+    create_cook_book()
+    get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
